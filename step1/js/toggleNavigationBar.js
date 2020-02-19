044c00d7bdacf872ef23b30162f32afe86ddc255
Harry Chiu <harrychiu1106@gmail.com>
2020-02-19 17:47:37 +0800

    Add archors

--- step1/js/toggleNavigationBar.js
+++ step1/js/toggleNavigationBar.js
@@ -1,13 +1,28 @@
+
+
 let collapse = false;
 
-let dropdown = document.querySelector("#nav-dropdown");
+hideDropdown = () => {
+    let dropdown = document.querySelector("#nav-dropdown");
+    dropdown.className = "nav-mobile__dropdown";
+    collapse = false;
+};
+
+showDropdown = () => {
+    let dropdown = document.querySelector("#nav-dropdown");
+    dropdown.className = "nav-mobile__dropdown nav-mobile__dropdown--active";
+    collapse = true;
+};
 
 let dropdownButton = document.querySelector("#nav-dropdown-button");
 dropdownButton.addEventListener("click", () => {
     if (collapse) {
-        dropdown.className = "nav-mobile__dropdown";
+        hideDropdown();
     } else {
-        dropdown.className = "nav-mobile__dropdown nav-mobile__dropdown--active";
+        showDropdown();
     };
     collapse = !collapse;
 });
+
+let navMobileButtons = document.querySelectorAll(".nav-mobile__item");
+navMobileButtons.forEach(button => button.addEventListener("click", hideDropdown));
